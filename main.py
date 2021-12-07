5e3a9c8b649464a4bb5daed7097cb5220f20dbe5
balker0322 <balker0322@gmail.com>
2021-12-07 17:24:18 +0800

    creating interface for risk manager

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from binance import Client
+from src.config import config
 
 
 def main():
-    print('first try')
+    mode = 'viewing'
+    api_key = config['binance_keys'][mode]['API_KEY']
+    api_secret = config['binance_keys'][mode]['SECRET_KEY']
+    client = Client(api_key, api_secret)
+
+    prices = client.get_all_tickers()
+
+    print(prices)
+
+
 
 
 if __name__=='__main__':
